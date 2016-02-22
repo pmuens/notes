e9b7eadfb3f139d7f505397dc8139ebe3047c82b
Philipp Muens <raute1337@gmx.de>
2016-02-22 14:07:01 +0100

    Add DynamoDB file

---
+++ nodejs/notes/lib/dynamo.js
@@ -0,0 +1,6 @@
+var AWS = require('aws-sdk');
+
+var dynamoDb = new AWS.DynamoDB({});
+
+module.exports.doc = new AWS.DynamoDB.DocumentClient({ service: dynamoDb });
+module.exports.tableName = process.env.SERVERLESS_PROJECT_NAME + '-notes-' + process.env.SERVERLESS_STAGE;
